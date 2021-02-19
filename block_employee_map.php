0086308b702d062d033e881d2841d490152afe16
Андрей Часников <chasnikov.aa@zhcom.ru>
2021-02-19 16:41:04 +0300

    Выгрузка проекта 19.02.2021 16:40; первая выгрузка

--- block_employee_map.php
+++ block_employee_map.php
@@ -39,65 +39,57 @@ class block_employee_map extends block_base {
       $html = '';
       $html .= '
       <div class="container-fluid">
-         <ul class="nav nav-tabs" id="employee_map_tab" role="tablist">
-            <li class="nav-item">
-               <a class="nav-link active" id="notstudied-tab" data-toggle="tab" href="#studied" role="tab" aria-controls="studied" aria-selected="true">Освоенные компетенции</a>
-            </li>
-            <li class="nav-item">
-               <a class="nav-link" id="notstudied-tab" data-toggle="tab" href="#notstudied" role="tab" aria-controls="notstudied" aria-selected="false">Неосвоенные компетенции</a>
-            </li>
-            <li class="nav-item">
-               <a class="nav-link" id="recommendation-tab" data-toggle="tab" href="#recommendation" role="tab" aria-controls="recommendation" aria-selected="false">Рекомендованные курсы и книги</a>
-            </li>
-         </ul>
-         <div class="tab-content" id="myTabContent">
-            <div class="tab-pane fade show active" id="studied" role="tabpanel" aria-labelledby="studied-tab">
-            <br>
-            Второй учебный план<br>
-            Направление развития<br>
-            Адаптационный план<br>
-            <br>
-            <a href="#">Ссылка1</a>
-            <br>
-            <a href="#">Ссылка2</a>
-            <br><br>
-            <h5><a href="#">Читаю книги</a> <span class="badge badge-secondary">3</span></h5>
-            <h5><a href="#">Книги без отчета</a> <span class="badge badge-secondary">2</span></h5>
-            <h5><a href="#">Значки</a> <span class="badge badge-secondary">5</span></h5>
-            </div>
-            <div class="tab-pane fade" id="notstudied" role="tabpanel" aria-labelledby="notstudied-tab">
-            <br>
-            <a href="#">Менеджер отдела продаж</a>
-            <br><br><br>
-            Hard Skills
-            <div class="progress">
-               <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-            <br>
-            Soft Skills
-            <div class="progress">
-               <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-            <br>
-            Все компетенции отдела
-            <div class="progress">
-               <div class="progress-bar bg-warning" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-            </div>
-            <br><br><br>
-            <div class="alert alert-primary" role="alert">
-               Дата сдачи учебного плана: 01.01.2021
-            </div>
-            </div>  
-            <div class="tab-pane fade" id="recommendation" role="tabpanel" aria-labelledby="recommendation-tab">              
-               <br>
-               <a href="#">Курс 1</a>
-              <br>
-              <a href="#">Курс 2</a>
-              <br><br>
-              <a href="#">Мои курсы</a>
-            </div>
-         </div>
+      <div class="row">
+        <div class="col-md-6 col-xl-4">
+          <h4>Освоенные компетенции</h4>
+          <br>
+          Второй учебный план<br>
+          Направление развития<br>
+          Адаптационный план<br>
+          <br>
+          <a href="#">Ссылка1</a>
+          <br>
+          <a href="#">Ссылка2</a>
+          <br><br>
+          <h5><a href="#">Читаю книги</a> <span class="badge badge-secondary">3</span></h5>
+          <h5><a href="#">Книги без отчета</a> <span class="badge badge-secondary">2</span></h5>
+          <h5><a href="#">Значки</a> <span class="badge badge-secondary">5</span></h5>        
+        </div>
+        <div class="col-md-6 col-xl-4">
+          <h4>Неосвоенные компетенции</h4>
+          <br>
+          <a href="#">Менеджер отдела продаж</a>
+          <br><br><br>
+          Hard Skills
+          <div class="progress">
+            <div class="progress-bar bg-success" role="progressbar" style="width: 25%" aria-valuenow="25" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+          <br>
+          Soft Skills
+          <div class="progress">
+            <div class="progress-bar bg-info" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>  
+          <br>
+          Все компетенции отдела
+          <div class="progress">
+            <div class="progress-bar bg-warning" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
+          </div>
+          <br><br><br>
+          <div class="alert alert-primary" role="alert">
+            Дата сдачи учебного плана: 01.01.2021
+          </div>        
+        </div>
+        <div class="col-md-6 col-xl-4">
+          <h4>Рекомендованные курсы и книги</h4>
+          <br>
+          <a href="#">Курс 1</a>
+          <br>
+          <a href="#">Курс 2</a>
+          <br><br>
+          <a href="#">Мои курсы</a>
+        </div>
       </div>
+    </div>
       ';
 
       $js = '';
